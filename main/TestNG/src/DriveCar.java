029617cdd53ef1c2692d3631d76ba3f6a5c5c98c
indian <indian@192.168.0.105>
2022-10-20 22:32:24 +0530

    Sample program explaining how to set priority

---
+++ main/TestNG/src/DriveCar.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class DriveCar {
+	@Test(priority=0)
+public void startTheCar() {
+	System.out.println("Start the car");
+}@Test(priority=1)
+public void firstGear() {
+	System.out.println("First gear");
+}@Test(priority=2)
+public void secondGear() {
+	System.out.println("Second gear");
+}@Test(priority=3)
+public void thirdGear() {
+	System.out.println("Third gear");
+}@Test(priority=4)
+public void fourthGear() {
+	System.out.println("Fourth gear");
+}
+}
